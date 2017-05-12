72c63500ed3690e02f6a4fd763285c017ae5df21
thiagopenido <tpenido@thoughtworks.com>
2017-05-12 15:48:54 -0300

    Creating fish

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Fish from './Fish'; 
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <Router>
+        <div className="App">
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Welcome to React</h2>
+          </div>
+          <p className="App-intro">
+            <button><Link to= "/fish">{'Start'}> </Link></button>
+          </p>
+        <Route path="/fish" component={Fish}/>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </Router>
     );
   }
 }
---
+++ src/Fish.js
@@ -0,0 +1,12 @@
+
+import React, { Component } from 'react';
+
+class Fish extends Component {
+  render() {
+    return (
+    <p>Fish</p>  
+    );
+  }
+}
+
+export default Fish;
